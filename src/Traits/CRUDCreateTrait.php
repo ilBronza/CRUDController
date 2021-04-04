e2163da473197840eeadcce56e666a7e9c5c9d98
Davide Bronzini <davide@sistema.srl>
2021-04-04 17:53:37 +0200

    create and store splitting

---
+++ src/Traits/CRUDCreateTrait.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace ilBronza\CRUD\Traits;
+
+use Illuminate\Http\Request;
+use ilBronza\Form\Facades\Form;
+
+trait CRUDCreateTrait
+{
+	use CRUDFormTrait;
+	use CRUDValidateTrait;
+
+	public $createView;
+	public $standardCreateView = 'form::uikit.form';
+
+	/**
+	 * get create view name
+	 *
+	 * if declared an overridden view return it, otherwise return default one
+	 *
+	 * @return string
+	 **/
+	public function getCreateView()
+	{
+		if($this->createView)
+			return $this->createView;
+
+		return $this->standardCreateView;
+	}
+
+	/**
+	 * get store model action form store form
+	 *
+	 * return [].store route with given model instance key
+	 *
+	 * @return string
+	 **/
+	public function getStoreModelAction()
+	{
+		return $this->getRouteUrlByType('store');
+	}
+
+	/**
+	 * share parameters to populate create view
+	 *
+	 * @return callable
+	 **/
+	public function shareDefaultCreateFormParameters()
+	{
+		return $this->shareDefaultFormParameters('create');
+	}
+
+	/**
+	 * overrideable method to manage modelInstance before view rendering
+	 **/
+	public function beforeRenderCreate() { }
+
+	/**
+	 * get modelInstance create view
+	 *
+	 * @return view
+	 **/
+	public function create()
+	{
+		$this->modelInstance = new $this->modelClass;
+
+		$this->manageParentModelAssociation();
+
+		$view = $this->getCreateView();
+
+		if($view == $this->standardCreateView)
+			$this->shareDefaultCreateFormParameters();
+
+		$this->beforeRenderCreate();
+
+		return view($view);
+	}
+}
---
+++ src/Traits/CRUDStoreTrait.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace ilBronza\CRUD\Traits;
+
+use Illuminate\Http\Request;
+use ilBronza\Form\Facades\Form;
+
+trait CRUDStoreTrait
+{
+	use CRUDFormTrait;
+	use CRUDValidateTrait;
+
+	/**
+	 * get after store redirect url
+	 *
+	 * @return string url
+	 */
+	public function getAfterStoredRedirectUrl()
+	{
+		return $this->getRouteUrlByType('index');
+	}
+
+	/**
+	 * get store validation array
+	 *
+	 * @return array
+	 **/
+	public function getStoreValidationArray()
+	{
+		return $this->getValidationArrayByType('store');
+	}
+
+	/**
+	 * validate request and return requested values for store
+	 **/
+	private function validateStoreRequest(Request $request)
+	{
+		return $this->validateRequestByType($request, 'store');
+	}
+
+	public function setBeforeStoreFields(array $parameters) { }
+
+	/**
+	 * store model instance with given array parameters
+	 *
+	 * @param array $parameters
+	 * @return boolean
+	 **/
+	public function storeModelInstance(array $parameters)
+	{
+		$parameters = array_diff_key($parameters, $this->getRelationshipsFieldsByType('store'));
+
+		foreach($parameters as $name => $value)
+			$this->modelInstance->$name = $value;
+
+		$this->setBeforeStoreFields($parameters);
+
+		$this->modelInstance->save();
+	}
+
+	/**
+	 * send store success message
+	 **/
+	public function sendStoreSuccessMessage()
+	{
+
+	}
+
+	/**
+	 * validate request and store model
+	 *
+	 * @param Request $request, Model $modelInstance
+	 * @return Response redirect
+	 **/
+	public function store(Request $request)
+	{
+		return $this->_store($request);
+	}
+
+	/**
+	 * validate request and store model
+	 *
+	 * @param Request $request, Model $modelInstance
+	 * @return Response redirect
+	 **/
+	public function _store(Request $request)
+	{
+		$parameters = $this->validateStoreRequest($request);
+
+		$parameters = $this->transformParametersByFieldsAndType($parameters, 'store');
+
+		$this->modelInstance = new $this->modelClass;
+
+		$this->manageParentModelAssociation();
+
+		$this->storeModelInstance($parameters);
+
+		// $this->modelInstance->save();
+		$this->associateRelationshipsByType($parameters, 'store');
+
+		$this->modelInstance->save();
+
+		$this->sendStoreSuccessMessage();
+
+		return redirect()->to(
+			$this->getAfterStoredRedirectUrl()
+		);
+	}
+}
