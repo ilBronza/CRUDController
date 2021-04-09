f03dbd25e46d44cd34b28456e5c8febc1aaea015
Vadym <vadymydav@gmail.com>
2021-04-09 15:25:33 +0200

    sistemato div chiusura con foreach all'interno

--- src/resources/views/partials/element.blade.php
+++ src/resources/views/partials/element.blade.php
@@ -2,22 +2,11 @@
 	<div class="uk-card uk-card-default uk-card-body uk-card-small">
 	    <span class="uk-sortable-handle uk-margin-small-right" uk-icon="icon: table"></span> {{ $element->getName() }}
 	</div>
-</div>
-
 @if ($element->children->count() > 0)
-<div class="group" uk-sortable="handle: .uk-sortable-handle; nestable: true; animation: 150; nestable-container-class: group">
-	@foreach($element->children as $_element)
+    <div class="group" uk-sortable="handle: .uk-sortable-handle; nestable: true; animation: 150; nestable-container-class: group">
+    @foreach($element->children as $_element)
         @include('crud::partials.element', ['element'=>$_element])
     @endforeach
-</div>
+    </div>
 @endif
-
-
-
-{{-- 	@if ($element->children->count() > 0)
-	    <div class="group" uk-sortable="handle: .uk-sortable-handle; nestable: true; animation: 150; nestable-container-class: group">
-	    @foreach($element->children as $_element)
-	        @include('crud::partials.element', ['element'=>$_element])
-	    @endforeach
-	    </div>
-	@endif --}}
+</div>
