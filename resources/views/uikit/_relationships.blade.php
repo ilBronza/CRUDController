58d991a167496fe60ea4b59ccdc43501d62fc9fa
Davide Bronzini <davide_bronzini@hotmail.com>
2023-05-25 14:38:05 +0200

    going to switcher relationships

--- resources/views/uikit/_relationships.blade.php
+++ resources/views/uikit/_relationships.blade.php
@@ -28,25 +28,6 @@
 
 <!--DEPRECATO A MANETTA-->
 
-
-
-@if(isset($relationshipManager))
-<div class="uk-card-footer">
-
-@foreach($relationshipManager->getRelationships() as $relationship)
-
-    <div class="uk-card uk-card-small">
-        <div class="uk-card-header">
-            <span class="uk-h3">
-                {!! $relationship->getCardTitle() !!}
-            </span>
-        </div>
-        <div class="uk-card-body">
-            {!! $relationship->render() !!}            
-        </div>
-    </div>
-@endforeach
-
-</div>
-
-@endif
+{{-- @include('crud::uikit.__relationshipsFlat') --}}
+@include('crud::uikit.__relationshipsToggler')
+{{-- @include('crud::uikit.__relationshipsSwitcher') --}}
